796b0f32dddedc22da95ceccfc78a19ee21e4c33
sincostancscseccot <cjtjtjvfyj@gmail.com>
2026-02-15 16:16:32 +0800

    Add ENABLE_FIREBASE option to blog configuration

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,8 @@ const BLOG = {
     'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
 
   // uuid重定向至 slug
-  UUID_REDIRECT: process.env.UUID_REDIRECT || false
+  UUID_REDIRECT: process.env.UUID_REDIRECT || false,
+  ENABLE_FIREBASE: false,
 }
 
 module.exports = BLOG
